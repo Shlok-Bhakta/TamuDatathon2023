7c185d57486fbbcfeb2018814314b0f0be91a08d
N1v3x2 <kevzhang2022@gmail.com>
2023-10-28 18:06:36 -0500

    - Added Taipy table

--- schedule_builder.py
+++ schedule_builder.py
@@ -1,12 +1,20 @@
 import taipy as tp
 from taipy import Gui
 import cohere
+import pandas as pd
 
 prompt = ('1. Do CS221 HW (most important) \n2. Read POLS206 textbook \n3. Workout \n4. Read HIST105 novel \n'
           '5. Prep for datathon (least important)')
 co = cohere.Client('s0Sws2fzSjJPeXu0zXwztHc9RisE1A8TVKxBkDgY')
 response = ''
 feedback = ''
+
+food_df = pd.DataFrame({
+    "Meal": ["Lunch", "Dinner", "Lunch", "Lunch", "Breakfast", "Breakfast", "Lunch", "Dinner"],
+    "Category": ["Food", "Food", "Drink", "Food", "Food", "Drink", "Dessert", "Dessert"],
+    "Name": ["Burger", "Pizza", "Soda", "Salad", "Pasta", "Water", "Ice Cream", "Cake"],
+    "Calories": [300, 400, 150, 200, 500, 0, 400, 500],
+})
 
 
 def generate_schedule(state):
@@ -54,6 +62,9 @@ Feedback on this schedule:
 <|{feedback}|input|multiline|> <br />
 <|Submit|button|on_action=submit_feedback|class_name=plain|>
 |>
+<br/>
+<|{food_df}|table|>
+
 """
 
 Gui(page).run(use_reloader=True, port=5001)
