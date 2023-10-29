7989a4997ae9c93af27728ff8cb55be68d0ed9b0
N1v3x2 <kevzhang2022@gmail.com>
2023-10-29 04:38:29 -0500

    - Remove unnecessary files

--- main.py
+++ main.py
@@ -1,60 +1,80 @@
-from taipy import Gui
+import taipy as tp
+from taipy.gui import Gui, notify
+import cohere
 import pandas as pd
 
-value = 10
+prompt = ('1. Do CS221 HW (most important) \n2. Read POLS206 textbook \n3. Workout \n4. Read HIST105 novel \n'
+          '5. Prep for datathon (least important)')
+co = cohere.Client('s0Sws2fzSjJPeXu0zXwztHc9RisE1A8TVKxBkDgY')
+response = ''
+feedback = ''
 
-section1 = """
-#Our Very First Taipy Application
+def generate_schedule(state):
+    state.response = co.generate(
+        prompt=f"""Generate a single-day schedule for me with the following priorities: {prompt} Only give me a list 
+               with times and tasks.""",
+        max_tokens=125
+    )[0].text
 
-<|layout|columns=1 1 1|
+
+def submit_feedback(state):
+    state.response = co.generate(
+        prompt=f"""Here is the schedule you gave me: \"{prompt}\"
+                Here is my feedback: {feedback}.
+                Regenerate the schedule with this feedback in mind; only give me a list with times and tasks.""",
+        max_tokens=125
+    )[0].text
+
+
+def food_df_on_edit(state, var_name, payload):
+    index = payload["index"] # row index
+    col = payload["col"] # column name
+    value = payload["value"] # new value cast to the column type
+    user_value = payload["user_value"] # new value as entered by the user
+
+    old_value = state.food_df.loc[index, col]
+    new_food_df = state.food_df.copy()
+    new_food_df.loc[index, col] = value
+    state.food_df = new_food_df
+    notify(state, "I", f"Edited value from '{old_value}' to '{value}'. (index '{index}', column '{col}')")
+
+
+data = {
+  "Country": ["Rest of the world","Russian Federation",...,"Peru"],
+  "Area": [1445674.66,815312,...,72330.4]
+}
+
+
+page = """
+###Scheduler Builder
+
+<|layout|columns=1 1|
 
 <|first column
-<|container container-styling|
-###Slider 1 <br/>
-Slider value: <|{value}|> <br/>
-<|{value}|slider|>
+<|container|
+Input your daily priorities: <br/>
+<|{prompt}|input|class_name=schedule-input|multiline|> <br/>
+<|Generate|button|on_action=generate_schedule|class_name=plain|>
 |>
 |>
 
 <|second column
-<|container container-styling|
-###Slider 2 <br/>
-Slider value: <|{value}|> <br/>
-<|{value}|slider|>
-|>
-|>
-
-<|third column
-<|container container-styling|
-###Slider 3 <br/>
-Slider value: <|{value}|> <br/>
-<|{value}|slider|>
+<|container|
+Generated Schedule: <br/>
+<|{response}|input|class_name=schedule-result|multiline|>
 |>
 |>
 
 |>
-"""
-section2 = """
-<|layout|columns=1 4|
 
-<|container container-styling|
-<|{content}|file_download|label=Download|>
+<br/>
+<|container|
+Feedback on this schedule:
+<|{feedback}|input|multiline|> <br />
+<|Submit|button|on_action=submit_feedback|class_name=plain|>
 |>
+<br/>
 
-<|{data}|chart|type=bar|x=Country|y=Average Age|orientation=v|>
-
-|>
 """
 
-
-def get_data(path: str):
-    dataset = pd.read_csv(path)
-    return dataset
-
-
-data = get_data("average_age.csv")
-content = "average_age.csv"
-
-Gui(page=section1+section2, css_file='main.css').run(use_reloader=True, port=5001)
-
-#Gui(page="Hello *World*").run(use_reloader=True, port=5001)
+Gui(page).run(use_reloader=True, port=5001)
